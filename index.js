281d00bf9c199da2a9940417af6079b6977334ab
lim_hangyoel <lim_hangyoel@radcns.com>
2020-11-25 13:50:50 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require("mongoose")
+mongoose.connect('mongodb+srv://escher:escher@boilerplate.2x61l.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
+}).then(() => console.log('mongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send("hello world"))
+app.listen(port, ()=>console.log(`port : ${port}`))
