ce5250da9afec25e871a33990162778654d6b9e5
scriabinEtude <be_dasan@naver.com>
2020-11-25 23:13:26 +0900

    12강까지

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
+const jwt = require("jsonwebtoken")
+
 
 const userSchema = mongoose.Schema({
     name : {
@@ -12,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     password : {
         type : String, 
-        maxlength : 5
+        minlength : 5
     },
     lastname : {
         type : String,
@@ -31,6 +35,45 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+userSchema.pre("save", function(next){
+    var user = this
+
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword 123123 | 암호화된 비밀번호 $2b$10$UfwJA3x17BMrSqO58h0APOwVh9GqK15G5sWZ5QMzoLIISH96TFAr2
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this
+    //jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign({data:user._id}, "secretToken")
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+    
+}
 
 const User = mongoose.model('User', userSchema)
 
