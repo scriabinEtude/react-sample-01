e499b5f7cbd7dd909fb459e6f91e628edd032305
lim_hangyoel <lim_hangyoel@radcns.com>
2020-11-27 14:44:29 +0900

    Lec.5

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+export const API_KEY = '1d0150e32451b718689ecb368ecead0e'
--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,8 @@
 module.exports = {
     mongoURI : "mongodb+srv://escher:escher@boilerplate.2x61l.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
+
+//themoviedb.org api key
+//1d0150e32451b718689ecb368ecead0e
+//https://api.themoviedb.org/3/movie/550?api_key=1d0150e32451b718689ecb368ecead0e
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZDAxNTBlMzI0NTFiNzE4Njg5ZWNiMzY4ZWNlYWQwZSIsInN1YiI6IjVmYzA1N2NiOWM5N2JkMDAzY2EyNGRkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ap4d2TL99kbotx3-LsooNwhrsRgsYb1O8NFjl_eqQHU
